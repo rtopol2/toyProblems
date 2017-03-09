23bfaa88df92522294b75f2b76c53ad460c36965
Ross Topol <rtopol2@gmail.com>
2017-03-09 14:00:06 -0600

    Begin preliminary solution

---
+++ Friends/Some Sums/Some Sums [solution].js
@@ -0,0 +1,48 @@
+// function someSums (array, sum, num) {
+//   for (let i=0; i<array.length; i++){
+//     let sub = []
+//     let j = i;
+//     while (sub.length < num) {
+//      console.log(sub)
+//      sub.push(array[j]);
+//      j++;
+//     }
+//     if (sub.length === num && getSum(sub) === sum) {
+//       return sub;
+//     }
+//   }
+// }
+
+function getSum(arr){
+  return arr.reduce((sum, num)=>sum+num);
+}
+
+
+unction someSums (array, sum, num) {
+
+let result = false;
+
+function recurse (arr, sub=[]) {
+  let tempArr = arr.slice();
+  let tempSub = sub.slice();
+  if (tempSub)
+  for (let num of arr){
+    if 
+  }
+}
+
+
+
+
+
+someSums([-1,-6,-2,-10,4,5, -1, -10, 1,-2], -10, 3)
+
+
+
+
+
+
+
+
+
+
